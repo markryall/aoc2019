b8508c604718040a348d6f13531fad6fe3611efb
Mark Ryall <mark@ryall.name>
2020-09-25 22:32:10 +1000

    fuel_fuel_fuel

--- lib/aoc2019/day1.ex
+++ lib/aoc2019/day1.ex
@@ -4,9 +4,17 @@ defmodule Aoc2019.Day1 do
     contents
     |> String.split("\n", trim: true)
     |> Enum.map(&String.to_integer/1)
-    |> Enum.map(&Aoc2019.Day1.fuel/1)
+    |> Enum.map(&Aoc2019.Day1.fuel_fuel/1)
     |> Enum.sum
     |> IO.inspect
+  end
+
+  def fuel_fuel(mass) do
+    fuel = fuel(mass)
+    case fuel do
+      n when n <= 0 -> 0
+      _ -> fuel + fuel_fuel(fuel)
+    end
   end
 
   def fuel(mass) do
--- test/aoc2019/day1_text.exs
+++ test/aoc2019/day1_text.exs
@@ -7,4 +7,10 @@ defmodule Aoc2019Test.Day1 do
     assert Aoc2019.Day1.fuel(1969) == 654
     assert Aoc2019.Day1.fuel(100756) == 33583
   end
+
+  test "calculates fuel fuel" do
+    assert Aoc2019.Day1.fuel_fuel(14) == 2
+    assert Aoc2019.Day1.fuel_fuel(1969) == 966
+    assert Aoc2019.Day1.fuel_fuel(100756) == 50346
+  end
 end
