55b9b7e60358b0f50aef5ca75dc0c9fec160bc39
Mark Ryall <mark@ryall.name>
2020-09-29 21:06:59 +1000

    day 4 part 2

--- lib/aoc2019/day4.ex
+++ lib/aoc2019/day4.ex
@@ -1,7 +1,7 @@
 defmodule Aoc2019.Day4 do
   def go() do
     Enum.map(183564..657474, fn n -> n end)
-    |> Enum.filter(&valid/1)
+    |> Enum.filter(&valid2/1)
     |> Enum.count
     |> IO.inspect
   end
@@ -9,5 +9,12 @@ defmodule Aoc2019.Day4 do
   def valid(n) do
     codepoints = n |> Integer.to_string |> String.codepoints
     codepoints == Enum.sort(codepoints) && Enum.count(Enum.into(codepoints, MapSet.new)) < 6
+  end
+
+  def valid2(n) do
+    codepoints = n |> Integer.to_string |> String.codepoints
+    distribution = codepoints |> Enum.reduce(%{}, fn char, acc -> Map.update(acc, char, 1, &(&1 + 1)) end)
+    pairs = Map.values(distribution) |> Enum.filter(fn count -> count == 2 end)
+    codepoints == Enum.sort(codepoints) && Enum.count(pairs) > 0
   end
 end
--- test/aoc2019/day4_test.exs
+++ test/aoc2019/day4_test.exs
@@ -1,9 +1,22 @@
 defmodule Aoc2019Test.Day4 do
   use ExUnit.Case
 
-  test "valid/invalid" do
+  test "valid" do
     assert Aoc2019.Day4.valid(111111) == true
     assert Aoc2019.Day4.valid(223450) == false
     assert Aoc2019.Day4.valid(123789) == false
+    assert Aoc2019.Day4.valid(112233) == true
+    assert Aoc2019.Day4.valid2(123444) == false
+    assert Aoc2019.Day4.valid2(111122) == true
+  end
+
+  test "valid2" do
+    assert Aoc2019.Day4.valid2(111111) == true
+    assert Aoc2019.Day4.valid2(223450) == false
+    assert Aoc2019.Day4.valid2(123789) == false
+    assert Aoc2019.Day4.valid2(112233) == true
+    assert Aoc2019.Day4.valid2(123444) == false
+    assert Aoc2019.Day4.valid2(111122) == true
+    assert Aoc2019.Day4.valid2(111222) == false
   end
 end
